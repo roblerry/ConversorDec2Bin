339a03f53bfa6011a7b86dbcc7818b78a34d91e0
roblerry <roblesmc0902@gmail.com>
2022-09-15 18:29:17 +0200

    se ha modificado!

--- dec2bin.py
+++ dec2bin.py
@@ -10,13 +10,22 @@ def dec2bin(numero_decimal, numero_bits):
     if numero_decimal >= 0
             numero_binario = numero_binario[2:len(numero_binario)] # quita el "0b" delprincipio
             while len(numero_binario) < numero_bits: # añade 0's a la izquierda si hacefalta
+    if numero_decimal >= 0:
+        numero_binario = numero_binario[2:len(numero_binario)] # quita el "0b" delprincipio
+        
+        while len(numero_binario) < numero_bits: # añade 0's a la izquierda si hacefalta
             numero_binario = "0" + numero_binario
     else:
         numero_binario = numero_binario[3:len(numero_binario)] # quita el "-0b" del principio
         while len(numero_binario) < numero_bits: # añade 1's a la izquierda si hace falta
             numero_binario = "1" + numero_binario
-    return numero_binario
+=======
+    numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
+    
+    while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
+        numero_binario = "0" + numero_binario
 
+    return numero_binario
 
 # ----------------------------------------
 # MAIN
